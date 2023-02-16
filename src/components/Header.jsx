d0f04a02f7cf6520f82c39ee2fc2e8c3145fd4d1
Md-Rohed <mdrohed85@gmail.com>
2023-02-16 17:19:39 +0600

    header Section added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,13 @@ import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div className="mx-auto flex justify-between  bg-white border p-6 w-4/5 mt-9"></div>
+    <div className="mx-auto flex justify-between  bg-white border p-6 w-4/5 mt-9">
+      <div className="text-2xl text-red-900 font-bold flex ">
+        <img className="h-10" src={logo}></img>
+        <span>Todo-Text</span>
+      </div>
+      <div className="text-2xl"> TODO-APP</div>
+    </div>
   );
 };
 
