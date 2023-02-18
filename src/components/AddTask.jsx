4935b3f18039b88c14a0d9e9c8871dee53449abb
Md-Rohed <mdrohed85@gmail.com>
2023-02-19 03:22:34 +0600

    ui updated

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const AddTask = () => {
-  return <div>AddTask</div>;
+  return (
+    <div className=" m-20 flex  place-content-center  ">
+      <input
+        className="bg-blue-500 w-96 rounded-lg placeholder-white text-white p-2 "
+        placeholder="add task"
+      ></input>
+      <button
+        className="bg-green-600 p-2 px-4 text-white border rounded-2xl"
+        type="submit"
+      >
+        Add
+      </button>
+    </div>
+  );
 };
 
 export default AddTask;
--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const ShowTask = () => {
-  return <div>ShowTask</div>;
+  const tasklist = [
+    { id: 10001, name: "TASK A ", time: "2:09:01 AM 09/03/2023" },
+    { id: 10001, name: "TASK A ", time: "2:09:01 AM 09/03/2023" },
+    { id: 10001, name: "TASK A ", time: "2:09:01 AM 09/03/2023" },
+  ];
+  return (
+    <div className="m-16 bg-slate-300 rounded-2xl h-96 ">
+      <div className="flex justify-between  ml-60  p-10">
+        <span className="font-bold text-2xl "> Todo</span>
+        <button className="bg-blue-600 text-white p-2  mr-60">Clear All</button>
+      </div>
+      <ul className="flex justify-between  mx-96">
+        {tasklist.map((task) => (
+          <li className="bg-white     p-12 px-16 rounded-xl font-bold uppercase">
+            <p className="flex   flex-col">
+              <span>{task.name}</span>
+              <span>{task.time} </span>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ShowTask;
